336bcfddc7ca9d284288fb45f63351bee670a18d
Joey <joey.xf@gmail.com>
2025-02-13 15:23:24 +0800

    add my comment chrome extension

--- astro.config.ts
+++ astro.config.ts
@@ -77,7 +77,7 @@ export default defineConfig({
 			[
 				rehypeExternalLinks,
 				{
-					rel: ["nofollow, noreferrer"],
+					rel: ["noreferrer", "noopener"],
 					target: "_blank",
 				},
 			],
